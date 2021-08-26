ed60e84edb02dbdf85a6233aee0617175cbfb8f0
Alex Kuang <17879507+alexkuang0@users.noreply.github.com>
2021-08-26 16:53:28 +0800

    update build.js

--- build.js
+++ build.js
@@ -4,7 +4,9 @@ build({
   entryPoints: ['./src/index.jsx'],
   outfile: './dist/index.js',
   inject: ['./scriptable-x-shim.js'],
-  jsxFactory: 'x'
+  jsxFactory: 'x',
+  jsxFragment: 'f',
+  bundle: true,
 }).catch(err => {
   process.stderr.write(err.stderr)
   process.exit(1)
