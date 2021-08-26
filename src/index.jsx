c41ddcfbe3880ab55a60c2fe5a751606a0a6df8d
Alex Kuang <17879507+alexkuang0@users.noreply.github.com>
2021-08-26 16:54:03 +0800

    update example

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
 function Text() {
-  return <text size="18">Hello World</text>
+  return (
+    <widget>
+      <text size="18">Hello World</text>
+      <text size="12"></text>
+    </widget>
+  )
 }
 
 render(<Text />)
